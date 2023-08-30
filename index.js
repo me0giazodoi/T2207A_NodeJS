5fe7491f4388e46e3a93d0ac2765cc046ea76c24
Windows10-DPC <hiepga1243@gmail.com>
2023-08-30 10:45:41 +0700

    abc

--- index.js
+++ index.js
@@ -2,17 +2,33 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.listen(port,function(){ //callback function
-    console.log("Sever is running...");
+app.listen(port,function(){ // callback function
+    console.log("Server is running...");
 })
 
-require("./src/db/database");
+// config session
+const session = require("express-session");
+app.use(
+    session({
+        resave: true,
+        saveUninitialized: true,
+        secret: "t2207a123abcxyz",
+        cookie: {
+            maxAge: 3600000, // miliseconds
+            secure: false
+        }
+    })
+);
+
+require("./src/db/datatabase");
 app.set("view engine","ejs");
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
 const webrouter = require("./src/routes/web");
-app.use("/",webrouter);
+app.use("/",webrouter); 
 const userrouter = require("./src/routes/user");
 app.use("/auth",userrouter);
+const productrouter = require("./src/routes/product.routes");
+app.use("/product",productrouter);
